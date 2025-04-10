58dcd182a5908b728760ff8bee7dee3c7624fee3
EliasTheGrandMasterOfMistakes <149844945+EliasTheGrandMasterOfMistakes@users.noreply.github.com>
2025-04-09 21:29:18 -0300

    script: revert to android-ndk-27
    Can fixes: 
    src/egl/meson.build:10:16: ERROR: Unknown variable "libpipe_loader_dynamic".

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -7,7 +7,7 @@ deps="meson ninja patchelf unzip curl pip flex bison zip"
 workdir="$(pwd)/turnip_workdir"
 magiskdir="$workdir/turnip_module"
 adrenotoolsdir="$workdir/turnip_adrenotools"
-ndkver="android-ndk-r28"
+ndkver="android-ndk-r27"
 sdkver="30"
 mesasrc="https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.zip"
 clear
